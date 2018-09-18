90c551da5c2eaf2f4b0f26ef7d866b146bd84a29
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-09-18 17:55:06 +0200

    Template for examples finished

--- jinjaExecutor.py
+++ jinjaExecutor.py
@@ -2,6 +2,42 @@
 from jinja2 import Template, Environment, FileSystemLoader
 import json
 
+
+def render_module(schema):
+  file_loader = FileSystemLoader('ansible_templates')
+  env = Environment (loader=file_loader, lstrip_blocks=False, trim_blocks=False)
+
+  module_name = "webfilter"
+  short_description = schema['schema']['help']
+  description = ""
+  path = schema['path']
+  name = schema['name']
+
+  current = schema
+
+  file = open('output/fortios_' + path + '_' + name + '.py', 'w')
+
+  template = env.get_template('doc.jinja')
+  output = template.render(**locals())
+  file.write(output)
+
+  template = env.get_template('examples.jinja')
+  output = template.render(**locals())
+  file.write(output)
+
+  template = env.get_template('return.jinja')
+  output = template.render(**locals())
+  file.write(output)
+
+  template = env.get_template('code.jinja')
+  output = template.render(**locals())
+  file.write(output)
+
+  print "File generated: " + 'output/fortios_' + path + '_' + name + '.py'
+
+  file.close()
+
+
 def function():
 
 
@@ -12,33 +48,15 @@ def function():
   for i, pn in enumerate(fgt_sch):
     if 'diagnose' not in pn['path'] and 'execute' not in pn['path']:
       print str(real_counter) + ": " + str(i)+": " + " " + pn['path']+'_'+pn['name']
+      render_module(fgt_sch[i])
       real_counter += 1
 
-
-  current = fgt_sch[191]
-
-  file_loader = FileSystemLoader('ansible_generator')
-  env = Environment (loader=file_loader, lstrip_blocks=False, trim_blocks=False)
-
-  template = env.get_template('doc.jinja')
-
-  module_name = "webfilter"
-  short_description = current['schema']['help']
-  description = ""
-  path = current['path']
-  name = current['name']
-
-  output = template.render(**locals())
-  print(output)
-
-  template = env.get_template('code.jinja')
-
-  output = template.render(**locals())
-  print(output)
-
-
 if __name__ == "__main__":
-    function()
+
+  function()
+#  fgt_schema_file = open('fgt_schema.json').read()
+#  fgt_sch = json.loads(fgt_schema_file)['results']
+#  render_module(fgt_sch[191])
 
   
 
