e65450f611b037d612289245645ea8c38ccaeba3
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-09-20 16:31:03 +0200

    Generate code fulfilling pep8

--- jinjaExecutor.py
+++ jinjaExecutor.py
@@ -1,68 +1,76 @@
 #!/usr/bin/python
 from jinja2 import Template, Environment, FileSystemLoader
 import json
+import autopep8
+import os
 
 
 def render_module(schema, version):
-  file_loader = FileSystemLoader('ansible_templates')
-  env = Environment (loader=file_loader, lstrip_blocks=False, trim_blocks=False)
 
-  short_description = schema['schema']['help']
-  description = ""
-  path = schema['path']
-  name = schema['name']
-  module_name = "fortios_" + path + "_" + name
+    file_loader = FileSystemLoader('ansible_templates')
+    env = Environment(loader=file_loader,
+                      lstrip_blocks=False, trim_blocks=False)
 
-  file = open('output/fortios_' + path + '_' + name + '.py', 'w')
-  file_example = open('examples/fortios_' + path + '_' + name + '_example.yml', 'w')
+    short_description = schema['schema']['help']
+    description = ""
+    path = schema['path']
+    name = schema['name']
+    module_name = "fortios_" + path + "_" + name
 
-  template = env.get_template('doc.jinja')
-  output = template.render(**locals())
-  file.write(output)
+    template = env.get_template('doc.jinja')
+    output = template.render(**locals())
 
-  template = env.get_template('examples.jinja')
-  output = template.render(**locals())
-  file.write(output)
-  lines = output.splitlines(True)
-  file_example.writelines(lines[2:-1])
+    template = env.get_template('examples.jinja')
+    output += template.render(**locals())
 
-  template = env.get_template('return.jinja')
-  output = template.render(**locals())
-  file.write(output)
+    template = env.get_template('return.jinja')
+    output += template.render(**locals())
 
-  template = env.get_template('code.jinja')
-  output = template.render(**locals())
-  file.write(output)
+    template = env.get_template('code.jinja')
+    output += template.render(**locals())
 
-  print "File generated: " + 'output/fortios_' + path + '_' + name + '.py'
+    dir = 'output/' + path
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-  file.close()
-  file_example.close()
+    file = open('output/' + path + '/fortios_' + path + '_' + name + '.py', 'w')
+    output = autopep8.fix_code(output)
+    file.write(output)
+    file.close()
+
+    file_example = open('output/' + path + '/fortios_' + path +
+                        '_' + name + '_example.yml', 'w')
+    template = env.get_template('examples.jinja')
+    output = template.render(**locals())
+    lines = output.splitlines(True)
+    file_example.writelines(lines[2:-1])
+    file_example.close()
+
+    print "\033[0mFile generated: " + 'output/\033[37mfortios_' + path + '_' + name + '.py'
+    print "\033[0mFile generated: " + 'output/\033[37mfortios_' + \
+        path + '_' + name + '_example.yml', 'w'
 
 
 def jinjaExecutor():
 
+    fgt_schema_file = open('fgt_schema.json').read()
+    fgt_schema = json.loads(fgt_schema_file)
+    fgt_sch_results = fgt_schema['results']
 
-  fgt_schema_file = open('fgt_schema.json').read()
-  fgt_schema = json.loads(fgt_schema_file)
-  fgt_sch_results = fgt_schema['results']
+    real_counter = 0
+    for i, pn in enumerate(fgt_sch_results):
+        if 'diagnose' not in pn['path'] and 'execute' not in pn['path']:
+            print '\n\033[0mParsing schema:'
+            print '\033[0mModule name: \033[92m' + pn['path']+'_'+pn['name']
+            print '\033[0mIteration:\033[93m' + str(real_counter) + "\033[0m, Schema position: \033[93m" + str(i)
+            render_module(fgt_sch_results[i], fgt_schema['version'])
+            real_counter += 1
 
-  real_counter = 0
-  for i, pn in enumerate(fgt_sch_results):
-    if 'diagnose' not in pn['path'] and 'execute' not in pn['path']:
-      print str(real_counter) + ": " + str(i)+": " + " " + pn['path']+'_'+pn['name']
-      render_module(fgt_sch_results[i], fgt_schema['version'])
-      real_counter += 1
 
 if __name__ == "__main__":
 
-  #fgt_schema_file = open('fgt_schema.json').read()
-  #fgt_schema = json.loads(fgt_schema_file)
-  #fgt_sch_results = fgt_schema['results']
-  #render_module(fgt_sch_results[191], fgt_schema['version'])
-  jinjaExecutor()
-
-  
-
-
-
+    fgt_schema_file = open('fgt_schema.json').read()
+    fgt_schema = json.loads(fgt_schema_file)
+    fgt_sch_results = fgt_schema['results']
+    render_module(fgt_sch_results[215], fgt_schema['version'])
+    jinjaExecutor()
