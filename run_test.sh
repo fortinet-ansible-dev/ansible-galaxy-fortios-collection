fcb996d9b2a0f78fb65d5c78f94ebf89a2df61bb
Miguel Angel Munoz <magonzalez@fortinet.com>
2019-02-06 13:29:09 +0100

    New module for Antivirus Profile

--- run_test.sh
+++ run_test.sh
@@ -77,6 +77,7 @@ function run_example( ) {
 }
 
 run_example antivirus fortios_antivirus_heuristic_example.yml
+run_example antivirus fortios_antivirus_profile_example.yml
 run_example application fortios_application_list_example.yml
 run_example firewall fortios_firewall_address_example.yml
 run_example firewall fortios_firewall_address6_example.yml
